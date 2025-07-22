23e9fde392415008fc3430ba50bce40394245dca
neylton.deus <neylton.deus@valecard.com.br>
2025-07-22 15:29:10 -0300

    Endpoint hello

---
+++ src/main/java/com/example/demo_api/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/hello")
+public class HelloController {
+
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
